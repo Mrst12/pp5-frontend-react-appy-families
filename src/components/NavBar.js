00ce45b4896d6f7ab191bafef763bd43a0ef93af
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-27 19:21:21 +0000

    Add font awesome link to index.html, add fonta wesome icons to navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,12 +13,24 @@ const NavBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto flex-column text-left">
-                            <Nav.Link>Memo</Nav.Link>
-                            <Nav.Link>To Do List</Nav.Link>
-                            <Nav.Link>Achievements</Nav.Link>
-                            <Nav.Link>Login</Nav.Link>
-                            <Nav.Link>Signup</Nav.Link>
-                            <Nav.Link>About</Nav.Link>
+                            <Nav.Link>
+                            <i class="far fa-sticky-note"></i> Memo
+                            </Nav.Link>
+                            <Nav.Link>
+                            <i className="fas fa-list"></i> To Do List
+                            </Nav.Link>
+                            <Nav.Link>
+                            <i className="fas fa-trophy"></i> Achievements
+                            </Nav.Link>
+                            <Nav.Link>
+                            <i className="fas fa-sign-in-alt"></i> Login
+                            </Nav.Link>
+                            <Nav.Link>
+                            <i className="fas fa-user-plus"></i> Signup
+                            </Nav.Link>
+                            <Nav.Link>
+                            <i className="fas fa-info-circle"></i> About
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
