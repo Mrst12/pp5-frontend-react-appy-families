e91bf43ef01b723205686a57c01fb92fe63272f6
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-27 10:23:46 +0000

    Add links for appy families to the navbar component to see the basic structure

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,27 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div>
+            <div fixed="top">
+                <h1>APPY FAMILIES</h1>
+            </div>
+            <Navbar bg="light" expand="md">
+                <Container>
+                    {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto flex-column text-left">
+                            <Nav.Link>Memo</Nav.Link>
+                            <Nav.Link>To Do List</Nav.Link>
+                            <Nav.Link>Achievements</Nav.Link>
+                            <Nav.Link>Login</Nav.Link>
+                            <Nav.Link>Signup</Nav.Link>
+                            <Nav.Link>About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     );
 };
 
