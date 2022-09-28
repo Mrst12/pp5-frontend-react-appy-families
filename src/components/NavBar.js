8c62c9340ab61c087a5a283621b22b142bdc0c3e
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-28 18:40:48 +0000

    change the nav.link to navlink so the routes from app.js file work properly, check they all take to correct link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -14,24 +15,24 @@ const NavBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto flex-column text-left">
-                            <Nav.Link>
+                            <NavLink to="/">
                             <i class="far fa-sticky-note"></i> Memo
-                            </Nav.Link>
-                            <Nav.Link>
+                            </NavLink>
+                            <NavLink to="/todo">
                             <i className="fas fa-list"></i> To Do List
-                            </Nav.Link>
-                            <Nav.Link>
+                            </NavLink>
+                            <NavLink to="/achievements">
                             <i className="fas fa-trophy"></i> Achievements
-                            </Nav.Link>
-                            <Nav.Link>
+                            </NavLink>
+                            <NavLink to="/login">
                             <i className="fas fa-sign-in-alt"></i> Login
-                            </Nav.Link>
-                            <Nav.Link>
+                            </NavLink>
+                            <NavLink to="/signup">
                             <i className="fas fa-user-plus"></i> Signup
-                            </Nav.Link>
-                            <Nav.Link>
+                            </NavLink>
+                            <NavLink to="about">
                             <i className="fas fa-info-circle"></i> About
-                            </Nav.Link>
+                            </NavLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
