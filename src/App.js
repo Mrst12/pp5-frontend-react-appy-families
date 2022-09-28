88bf44d07acacfa460518c8a63a1aa2891063b20
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-28 10:51:51 +0000

    Import container, route and switch, add route paths for the various pages in app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       < NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Memo page</h1>} />
+          <Route exact path="/todo" render={() => <h1>To Do List</h1>} />
+          <Route exact path="/achievements" render={() => <h1>Achievements</h1>} />
+          <Route exact path="/signup" render={() => <h1>Signup</h1>} />
+          <Route exact path="/about" render={() => <h1>About</h1>} />
+          <Route exact path="/login" render={() => <h1>Login</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
