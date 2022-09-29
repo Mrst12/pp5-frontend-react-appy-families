63fd75827d91678b9f3fb911f56ea5633c2e41f7
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-29 11:18:26 +0000

    Add pages and auth folders to files structure, create a signup form file in the auth folder, add startup boilerplate code ready for modification, add route to app.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import {Route, Switch} from 'react-router-dom';
 import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Memo page</h1>} />
           <Route exact path="/todo" render={() => <h1>To Do List</h1>} />
           <Route exact path="/achievements" render={() => <h1>Achievements</h1>} />
-          <Route exact path="/signup" render={() => <h1>Signup</h1>} />
+          <Route exact path="/signup" render={() =><SignUpForm />} />
           <Route exact path="/about" render={() => <h1>About</h1>} />
           <Route exact path="/login" render={() => <h1>Login</h1>} />
           <Route render={()=><p>Page not found!</p>} />
