f3e9a877cc4cab26935b297829307a12b807ea05
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-30 19:01:32 +0000

    add code needed for the request interceptor using axios

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
-import { axiosRes } from '../api/axiosDefaults';
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
 import { response } from 'msw';
 import { useHistory } from 'react-router-dom';
 
@@ -28,6 +28,26 @@ export const CurrentUserProvider = ({children}) => {
     }, []);
 
     useMemo(() => {
+      axiosReq.interceptors.request.use(
+        async (config) => {
+          try {
+            await axios.post('/dj-rest-auth/token/refresh/')
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push('/login')
+              }
+              return null;
+            });
+            return config;
+          }
+          return config;
+        },
+        (err) => {
+          return Promise.reject(err);
+        }
+      );
+      
       axiosRes.interceptors.response.use(
         (response) => response,
         async (err) => {
@@ -47,7 +67,7 @@ export const CurrentUserProvider = ({children}) => {
           return Promise.reject(err);
         }
       )
-    })
+    }, [history]);
 
     return (
         <CurrentUserContext.Provider value={currentUser}>
