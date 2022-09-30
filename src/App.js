8c8e174e1da3e0f1afe29ce1252415ea93412993
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-09-30 12:45:03 +0000

    refactor the current user context hook to take it out of app.js into its own folder, adding it to index.js so we can use it globally, export into signin and navbar pages

--- src/App.js
+++ src/App.js
@@ -5,47 +5,24 @@ import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
-import { createContext, useEffect, useState } from 'react';
-import axios from 'axios';
-
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get('dj-rest-auth/user/')
-      setCurrentUser(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    handleMount()
-  }, [])
-
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
-        <div className={styles.App}>
-          < NavBar />
-          <Container>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Memo page</h1>} />
-              <Route exact path="/todo" render={() => <h1>To Do List</h1>} />
-              <Route exact path="/achievements" render={() => <h1>Achievements</h1>} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route exact path="/about" render={() => <h1>About</h1>} />
-              <Route exact path="/login" render={() => <SignInForm />} />
-              <Route render={() => <p>Page not found!</p>} />
-            </Switch>
-          </Container>
-        </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
+    <div className={styles.App}>
+      < NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Memo page</h1>} />
+          <Route exact path="/todo" render={() => <h1>To Do List</h1>} />
+          <Route exact path="/achievements" render={() => <h1>Achievements</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/about" render={() => <h1>About</h1>} />
+          <Route exact path="/login" render={() => <SignInForm />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+    </div>
+
   );
 }
 
