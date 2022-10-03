cf71b4a98bda67bcf1bb6a4df85561045d8fb6c2
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-03 10:48:58 +0000

    set up achievement.js file, add basic placeholder code and link it into our achievements page display

---
+++ src/pages/achievements/Achievements.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../styles/Achievement.module.css';
+
+const Achievements = (props) => {
+    return (
+        <div>
+           Achievement placeholder text 
+        </div>
+    )
+}
+
+export default Achievements
--- src/pages/achievements/AchievementsPage.js
+++ src/pages/achievements/AchievementsPage.js
@@ -8,6 +8,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Achievements from "./Achievements";
 
 function AchievementsPage() {
     const { id } = useParams();
@@ -34,7 +35,7 @@ function AchievementsPage() {
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles for mobile</p>
-                <p>Post component</p>
+                <Achievements {...achievement.results [0]} setAchievement={setAchievement} />
                 <Container className={appStyles.Content}>
                     Comments
                 </Container>
