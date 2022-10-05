ba8d9f5c1ce64dc59d571bad31a4abab3041f009
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-05 18:38:09 +0000

    create the moredropdown custom component with its stylesheet, add the basic code needed for the component to run

---
+++ src/components/MoreDropdown.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import styles from '../styles/MoreDropdown.module.css';
+
+
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+    <i
+        className='fas fa-ellipsis-v'
+        ref={ref}
+        onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+        }}
+    />
+));
+
+export const MoreDropdown = () => {
+    return (
+        <Dropdown>
+            <Dropdown.Toggle as={ThreeDots} id="dropdown-custom-components">
+                Custom toggle
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+                <Dropdown.Item eventKey="3" active>
+                    Orange
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+  );
+};
