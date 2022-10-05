e8fe65ca0428f8b19f6ee60c59d6caee1273b5bd
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-05 09:33:33 +0000

    in Achievementspost page, destructure state, set up the use effect hook, add logic to the return statement to display all posts, allowing users to like and unlike posts

--- src/pages/achievements/AchievementsPostsPage.js
+++ src/pages/achievements/AchievementsPostsPage.js
@@ -1,5 +1,5 @@
 //display all achievements page
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -8,14 +8,54 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/AchievementsPosts.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Achievements from "./Achievements";
+import Asset from "../../components/Asset";
+import NoResults from "../../assets/no-results.png";
 
-function AchievementsPostsPage() {
-  
+function AchievementsPostsPage(message) {
+
+  const [achievementPosts, setAchievementPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchAchievementPosts = async () => {
+      try {
+        const { data } = await axiosReq.get('/achievements/');
+        setAchievementPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchAchievementPosts()
+  }, [pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        {hasLoaded ? (
+          <>
+            {achievementPosts.results.length ? (
+              achievementPosts.results.map((achievement_post) => (
+                <Achievements key={achievement_post.id} {...achievement_post} setAchievement={setAchievementPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container classNmae={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
