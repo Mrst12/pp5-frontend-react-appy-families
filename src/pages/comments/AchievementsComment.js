5338e2191cf08d03e9047d3ed2ba46b33994af84
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-07 08:48:26 +0000

    add the dropdown component to the achievements coments page as the first step to allowing user to control their own comment

--- src/pages/comments/AchievementsComment.js
+++ src/pages/comments/AchievementsComment.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Avatar from '../../components/Avatar';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from '../../styles/AchievementsComments.module.css';
 
 const AchievementsComment = (props) => {
@@ -12,6 +14,9 @@ const AchievementsComment = (props) => {
         created_on,
         content,
     } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
 
     return (
         <div>
@@ -25,6 +30,12 @@ const AchievementsComment = (props) => {
                     <span className={styles.Date}>{created_on}</span>
                     <p>{content}</p>
                 </Media.Body>
+                {is_owner && (
+                    <MoreDropdown
+                        handleEdit={() => {}}
+                        handleDelete={() => {}}
+                    />
+                )}
             </Media>
         </div>
     );
