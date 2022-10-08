4ba504e07d9bf0db92c57a6fa1d63f4e1ed54c91
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-08 19:07:35 +0000

    write the api request within the use effect hook for the profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -9,64 +9,86 @@ import Asset from "../../components/Asset";
 import styles from "../../styles/ProfilePage.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import {
+    useProfileData,
+    useSetProfileData,
+} from "../../contexts/ProfileDataContext";
 import FamilyProfiles from "./FamilyProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
+
+
 function ProfilePage() {
-  const [hasLoaded, setHasLoaded] = useState(false);
-  const currentUser = useCurrentUser();
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const currentUser = useCurrentUser();
+    const { id } = useParams();
 
-  useEffect(() => {
-      setHasLoaded(true);
-  }, [])
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [{ data: pageProfile }] = await Promise.all([
+                    axiosReq.get(`/profiles/${id}/`)
+                ])
+                setProfileData(prevState => ({
+                    ...prevState,
+                    pageProfile: { results: [pageProfile] }
+                }))
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchData()
+    }, [id, setProfileData]);
 
-  const mainProfile = (
-    <>
-      <Row noGutters className="px-3 text-center">
-        <Col lg={3} className="text-lg-left">
-          <p>Image</p>
-        </Col>
-        <Col lg={6}>
-          <h3 className="m-2">Profile username</h3>
-          <p>Profile stats</p>
-        </Col>
-        <Col lg={3} className="text-lg-right">
-        <p>Follow button</p>
-        </Col>
-        <Col className="p-3">Profile content</Col>
-      </Row>
-    </>
-  );
+    const mainProfile = (
+        <>
+            <Row noGutters className="px-3 text-center">
+                <Col lg={3} className="text-lg-left">
+                    <p>Image</p>
+                </Col>
+                <Col lg={6}>
+                    <h3 className="m-2">Profile username</h3>
+                    <p>Profile stats</p>
+                </Col>
+                <Col lg={3} className="text-lg-right">
+                    <p>Follow button</p>
+                </Col>
+                <Col className="p-3">Profile content</Col>
+            </Row>
+        </>
+    );
 
-  const mainProfilePosts = (
-    <>
-      <hr />
-      <p className="text-center">Profile owner's posts</p>
-      <hr />
-    </>
-  );
+    const mainProfilePosts = (
+        <>
+            <hr />
+            <p className="text-center">Profile owner's posts</p>
+            <hr />
+        </>
+    );
 
-  return (
-    <Row>
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <FamilyProfiles mobile />
-        <Container className={appStyles.Content}>
-          {hasLoaded ? (
-            <>
-              {mainProfile}
-              {mainProfilePosts}
-            </>
-          ) : (
-            <Asset spinner />
-          )}
-        </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        <FamilyProfiles />
-      </Col>
-    </Row>
-  );
+    return (
+        <Row>
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <FamilyProfiles mobile />
+                <Container className={appStyles.Content}>
+                    {hasLoaded ? (
+                        <>
+                            {mainProfile}
+                            {mainProfilePosts}
+                        </>
+                    ) : (
+                        <Asset spinner />
+                    )}
+                </Container>
+            </Col>
+            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+                <FamilyProfiles />
+            </Col>
+        </Row>
+    );
 }
 
 export default ProfilePage;
