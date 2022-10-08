2548da00a4378ed403fd84469605087c2cfe07d9
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-08 09:33:00 +0000

    add a family profiles file to display the family profiles, import it into the achievements page as a tester to make sure its all working

--- src/pages/achievements/AchievementsPostsPage.js
+++ src/pages/achievements/AchievementsPostsPage.js
@@ -15,6 +15,7 @@ import Asset from "../../components/Asset";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import FamilyProfiles from "../profiles/FamilyProfiles";
 
 function AchievementsPostsPage(message) {
 
@@ -92,7 +93,7 @@ function AchievementsPostsPage(message) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <FamilyProfiles />
       </Col>
     </Row>
   );
---
+++ src/pages/profiles/FamilyProfiles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import appStyles from '../../App.module.css';
+
+const FamilyProfiles = () => {
+    return (
+        <Container className={appStyles.Content}>
+            <p>Family profiles</p>
+        </Container>
+    )
+}
+
+export default FamilyProfiles
