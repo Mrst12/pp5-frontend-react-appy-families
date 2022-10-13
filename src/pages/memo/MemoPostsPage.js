4d11e7513629c8831f2f31e89c2981bf46554e08
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-13 07:50:50 +0000

    add the search functionality for memo posts including a timer to make it a better user experience

--- src/pages/memo/MemoPostsPage.js
+++ src/pages/memo/MemoPostsPage.js
@@ -18,10 +18,12 @@ function MemoPostsPage({ message }) {
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
 
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
         const fetchMemoPosts = async () => {
             try {
-                const { data } = await axiosReq.get('/memo_posts/')
+                const { data } = await axiosReq.get(`/memo_posts/?search=${query}`);
                 setMemoPost(data);
                 setHasLoaded(true);
             } catch (err) {
@@ -30,13 +32,33 @@ function MemoPostsPage({ message }) {
         }
 
         setHasLoaded(false);
-        fetchMemoPosts();
-    }, [pathname]);
+        const timer = setTimeout(() => {
+            fetchMemoPosts();
+        }, 1000);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [query, pathname]);
 
     return (
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles mobile</p>
+                <i className={`fas fa-search ${styles.SearchIcon}`} />
+                <Form
+                    className={styles.SearchBar}
+                    onSubmit={(event => event.preventDefault() )}
+                >
+                    <Form.Control
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                        type="text"
+                        className="mr-sm-2"
+                        placeholder="Search memo's"
+                        aria-label="search memos"
+
+                    />
+                </Form>
                 {hasLoaded ? (
                     <>
                         {memo_post.results.length ? (
