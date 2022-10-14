ad6b8145a2263fe1c97ff21aa77ab12ab4e74346
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-14 13:03:17 +0000

    create the comment edit form for memos and import it into memoComment.js so users can edit their own comments

--- src/pages/comments/MemoComment.js
+++ src/pages/comments/MemoComment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { axiosRes } from '../../api/axiosDefaults';
@@ -6,6 +6,7 @@ import Avatar from '../../components/Avatar';
 import { MoreDropdown } from '../../components/MoreDropdown';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from "../../styles/Comment.module.css";
+import MemoCommentEditForm from './MemoCommentEditForm.js';
 
 const MemoComment = (props) => {
     const {
@@ -19,6 +20,8 @@ const MemoComment = (props) => {
         setComments,
     } = props;
 
+    const [showEditForm, setShowEditForm] = useState(false);
+
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
 
@@ -30,7 +33,7 @@ const MemoComment = (props) => {
                     ...prevMemo.results[0],
                     comments_count: prevMemo.results[0].comments_count - 1
                 },
-            ],
+                ],
             }));
 
             setComments((prevComments) => ({
@@ -43,23 +46,37 @@ const MemoComment = (props) => {
     }
 
     return (
-        <div>
+        <>
             <hr />
             <Media>
                 <Link to={`/profiles/${profile_id}`}>
                     <Avatar src={profile_image} />
                 </Link>
-                <Media.Body className='align-self-center ml-2'>
+                <Media.Body className="align-self-center ml-2">
                     <span className={styles.Owner}>{owner}</span>
                     <span className={styles.Date}>{created_on}</span>
-                    <p>{content}</p>
+                    {showEditForm ? (
+                        <MemoCommentEditForm
+                            id={id}
+                            profile_id={profile_id}
+                            content={content}
+                            profileImage={profile_image}
+                            setComments={setComments}
+                            setShowEditForm={setShowEditForm}
+                        />
+                    ) : (
+                        <p>{content}</p>
+                    )}
                 </Media.Body>
-                {is_owner && (
-                    <MoreDropdown handleEdit={() => { }} handleDelete={handleDelete} />
+                {is_owner && !showEditForm && (
+                    <MoreDropdown
+                        handleEdit={() => setShowEditForm(true)}
+                        handleDelete={handleDelete}
+                    />
                 )}
             </Media>
-        </div>
-    )
+        </>
+    );
 }
 
 export default MemoComment;
