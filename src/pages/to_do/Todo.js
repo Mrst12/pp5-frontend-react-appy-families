c911673249e06c2dbafbd8db0eb32842292dae40
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-15 19:01:08 +0000

    add the owner and avatar to todo appand link it to owners profile page

--- src/pages/to_do/Todo.js
+++ src/pages/to_do/Todo.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Card, Media } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Avatar from '../../components/Avatar';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from '../../styles/Todo.module.css';
 
 const Todo = (props) => {
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        task_title,
+        created_on,
+        due_date,
+        content,
+        status,
+        urgent,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
     return (
-        <div>Todo placeholder text to check</div>
+        <Card className={styles.Post}>
+            <Card.Body>
+                <Media className='align-items-center justify-content-between'>
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profile_image} height={55} />
+                        {owner}
+                    </Link>
+                </Media>
+            </Card.Body>
+        </Card>
     )
 }
 
