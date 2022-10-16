d3e73ad1df0cea4b211b009a805e0be288261601
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-16 12:46:37 +0000

    set the useeffect hook up and set up the ternary statements inside the return block to show the users the right thing on the task page

--- src/pages/to_do/TodoPostsPage.js
+++ src/pages/to_do/TodoPostsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,20 +7,59 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/TodoPosts.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Todo from "./Todo";
+import NoResults from "../../assets/no-results.png";
+import Asset from "../../components/Asset";
 
-function TodoPostsPage() {
-  
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
-      </Col>
-    </Row>
-  );
+function TodoPostsPage({ message }) {
+    const [todo, setTodo] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fetchTodoPosts = async () => {
+            try {
+                const { data } = await axiosReq.get('/to_do/');
+                setTodo(data);
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        setHasLoaded(false);
+        fetchTodoPosts();
+    }, [pathname]);
+
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <p>Popular profiles mobile</p>
+                {hasLoaded ? (
+                    <>
+                        {todo.results.length ? (
+                            todo.results.map((todo) => (
+                                <Todo key={todo.id} {...todo} setTodo={setTodo} />
+                            ))
+                        ) : (
+                            <Container className={appStyles.Content}>
+                                <Asset src={NoResults} message={message} />
+                            </Container>
+                        )}
+                    </>
+                ) : (
+                    <Container className={appStyles.Content}>
+                        <Asset spinner />
+                    </Container>
+                )}
+            </Col>
+            <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+                <p>Popular profiles for desktop</p>
+            </Col>
+        </Row>
+    );
 }
 
 export default TodoPostsPage;
