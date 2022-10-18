92c76056789c35e8e34383e29cf22a8ce240c807
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-18 10:20:05 +0000

    change the layout and styling of the achievements edit page

--- src/pages/achievements/AchievementEditForm.js
+++ src/pages/achievements/AchievementEditForm.js
@@ -83,87 +83,79 @@ function AchievementEditForm() {
         }
     }
 
-    const textFields = (
-        <div className="text-center">
-            <Form.Group>
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="title"
-                    aria-label="title"
-                    value={title}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors?.title?.map((message, idx) => (
-                <Alert variant="warning" key="{idx}">
-                    {message}
-                </Alert>
-            ))}
-            <Form.Group>
-                <Form.Label>Content</Form.Label>
-                <Form.Control
-                    as="textarea"
-                    rows={6}
-                    name="content"
-                    aria-label="content"
-                    value={content}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors?.content?.map((message, idx) => (
-                <Alert variant="warning" key="{idx}">
-                    {message}
-                </Alert>
-            ))}
-
-            <Button
-                className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => history.goBack()}
-            >
-                Cancel
-            </Button>
-            <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-                Save
-            </Button>
-        </div>
-    );
-
     return (
-        <Form onSubmit={handleSubmit}>
-            <Row>
-                <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-                    <Container
-                        className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-                    >
-                        <Form.Group className="text-center">
-                            <figure>
-                                <Image className={appStyles.Image} src={image} rounded />
-                            </figure>
-                            <div>
-                                <Form.Label
-                                    className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
-                                    htmlFor="image-upload"
-                                >
-                                    Change the image
-                                </Form.Label>
-                            </div>
+        <Container className={styles.Container}>
+            <br />
+            <h2>
+                Create Achievement
+            </h2>
+            <br />
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="title"
+                        aria-label="title"
+                        value={title}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                {errors?.title?.map((message, idx) => (
+                    <Alert variant="warning" key="{idx}">
+                        {message}
+                    </Alert>
+                ))}
+                <Form.Group>
+                    <Form.Label>Content</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={6}
+                        name="content"
+                        aria-label="content"
+                        value={content}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+                {errors?.content?.map((message, idx) => (
+                    <Alert variant="warning" key="{idx}">
+                        {message}
+                    </Alert>
+                ))}
+                <Form.Group className="text-center">
+                    <figure>
+                        <Image className={appStyles.Image} src={image} rounded />
+                    </figure>
+                    <div>
+                        <Form.Label
+                            className={btnStyles.Button}
+                            htmlFor="image-upload"
+                        >
+                            Change the image
+                        </Form.Label>
+                    </div>
 
-                            <Form.File
-                                id="image-upload"
-                                accept="image/*"
-                                onChange={handleChangeImage}
-                                ref={imageInput}
-                            />
-                        </Form.Group>
-                        <div className="d-md-none">{textFields}</div>
-                    </Container>
-                </Col>
-                <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-                    <Container className={appStyles.Content}>{textFields}</Container>
-                </Col>
-            </Row>
-        </Form>
+                    <Form.File
+                        id="image-upload"
+                        accept="image/*"
+                        onChange={handleChangeImage}
+                        ref={imageInput}
+                    />
+                </Form.Group>
+                <div className="text-center">
+                    <Button
+                        className={btnStyles.Button}
+                        onClick={() => history.goBack()}
+                    >
+                        Cancel
+                    </Button>
+                    <Button className={btnStyles.Button} type="submit">
+                        Save
+                    </Button>
+                </div>
+            </Form>
+            <br />
+        </Container>
     );
 }
 
