49f91c30b194415378c95a65795b53decf829011
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-18 20:29:11 +0000

    change styling on achievements page so there is a gap between cards

--- src/pages/achievements/Achievements.js
+++ src/pages/achievements/Achievements.js
@@ -74,7 +74,7 @@ const Achievements = (props) => {
     }
 
 
-    return <Card className={styles.Achievements}>
+    return <Card className={styles.Post}>
         <Card.Body>
             <Media className="align-items-center justify-content-between">
                 <Link to={`/profiles/${profile_id}`}>
