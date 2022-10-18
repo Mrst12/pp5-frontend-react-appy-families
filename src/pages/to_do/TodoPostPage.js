9e115d43e6aa4115c791db55f1b0772664a9985f
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-18 07:25:06 +0000

    update the layout of the return statement for a better layout in todo single posts page

--- src/pages/to_do/TodoPostPage.js
+++ src/pages/to_do/TodoPostPage.js
@@ -10,37 +10,31 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Todo from "./Todo";
 
 function TodoPostPage() {
-    const { id } = useParams();
-    const [todo, setTodo] = useState({ results: [] });
+  const { id } = useParams();
+  const [todo, setTodo] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const [{data: todo}] = await Promise.all([
-                    axiosReq.get(`/to_do/${id}`),
-                ]);
-                setTodo({results: [todo]});
-                console.log(todo);
-            } catch (err) {
-                console.log(err);
-            }
-        };
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: todo }] = await Promise.all([
+          axiosReq.get(`/to_do/${id}`),
+        ]);
+        setTodo({ results: [todo] });
+        console.log(todo);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-        handleMount();
-    }, [id]);
-  
+    handleMount();
+  }, [id]);
+
 
 
   return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        <Todo {...todo.results[0]} setTodo={setTodo} TodoPostPage />
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
-      </Col>
-    </Row>
+    <Container>
+      <Todo {...todo.results[0]} setTodo={setTodo} TodoPostPage />
+    </Container>
   );
 }
 
