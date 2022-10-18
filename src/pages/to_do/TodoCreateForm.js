e058381d97f01632a94e8a54ad46c046a1d57a39
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-10-18 13:15:23 +0000

    alter layout and styling for the to do list create form page

--- src/pages/to_do/TodoCreateForm.js
+++ src/pages/to_do/TodoCreateForm.js
@@ -59,100 +59,7 @@ function TodoCreateForm() {
 
   const textFields = (
     <div className="text-center">
-      <Form.Group>
-        <Form.Label>Task Title:</Form.Label>
-        <Form.Control
-          type="text"
-          name="task_title"
-          value={task_title}
-          onChange={handleChange}
-          aria-label="task title"
-        />
-      </Form.Group>
-      {errors?.task_title?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-      <Form.Group>
-        <Form.Label>Due date:</Form.Label>
-        <Form.Control
-          type="date"
-          name="due_date"
-          value={due_date}
-          onChange={handleChange}
-          aria-label="due date"
-        />
-      </Form.Group>
-      {errors?.due_date?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-      <Form.Group>
-        <Form.Label>Task:</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={6}
-          name="content"
-          value={content}
-          onChange={handleChange}
-          aria-label="content"
-        />
-      </Form.Group>
-      {errors?.content?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-      <Form.Group>
-        <Form.Label>pending:</Form.Label>
-        <Form.Control
-          type="checkbox"
-          name="status"
-          value={status}
-          handleChange={handleChange}
-          aria-label="status"
-        />
 
-        <Form.Label>started:</Form.Label>
-        <Form.Control
-          type="checkbox"
-          name="status"
-          value={status}
-          handleChange={handleChange}
-          aria-label="status"
-        />
-
-        <Form.Label>done:</Form.Label>
-        <Form.Control
-          type="checkbox"
-          name="status"
-          value={status}
-          handleChange={handleChange}
-          aria-label="status"
-        />
-      </Form.Group>
-      {errors?.status?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
-      <Form.Group>
-        <Form.Label>Urgent</Form.Label>
-        <Form.Control
-          type="checkbox"
-          name="urgent"
-          value={urgent}
-          handleChange={handleChange}
-          aria-label="is it urgent"
-        />
-      </Form.Group>
-      {errors?.urgent?.map((message, idx) => (
-        <Alert variant="warning" key={idx}>
-          {message}
-        </Alert>
-      ))}
 
 
 
@@ -169,13 +76,121 @@ function TodoCreateForm() {
   );
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Row>
-        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-          <Container className={appStyles.Content}>{textFields}</Container>
-        </Col>
-      </Row>
-    </Form>
+    <Container className={styles.Container}>
+      <br />
+      <h2>Create a Task</h2>
+      <br />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Task Title:</Form.Label>
+          <Form.Control
+            type="text"
+            name="task_title"
+            value={task_title}
+            onChange={handleChange}
+            aria-label="task title"
+          />
+        </Form.Group>
+        {errors?.task_title?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+        <Form.Group>
+          <Form.Label>Due date:</Form.Label>
+          <Form.Control
+            type="date"
+            name="due_date"
+            value={due_date}
+            onChange={handleChange}
+            aria-label="due date"
+          />
+        </Form.Group>
+        {errors?.due_date?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+        <Form.Group>
+          <Form.Label>Task:</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={6}
+            name="content"
+            value={content}
+            onChange={handleChange}
+            aria-label="content"
+          />
+        </Form.Group>
+        {errors?.content?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+        <Form.Group>
+          <Form.Label>pending:</Form.Label>
+          <Form.Control
+            type="checkbox"
+            name="status"
+            value={status}
+            handleChange={handleChange}
+            aria-label="status"
+          />
+
+          <Form.Label>started:</Form.Label>
+          <Form.Control
+            type="checkbox"
+            name="status"
+            value={status}
+            handleChange={handleChange}
+            aria-label="status"
+          />
+
+          <Form.Label>done:</Form.Label>
+          <Form.Control
+            type="checkbox"
+            name="status"
+            value={status}
+            handleChange={handleChange}
+            aria-label="status"
+          />
+        </Form.Group>
+        {errors?.status?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+        <Form.Group>
+          <Form.Label>Urgent</Form.Label>
+          <Form.Control
+            type="checkbox"
+            name="urgent"
+            value={urgent}
+            handleChange={handleChange}
+            aria-label="is it urgent"
+          />
+        </Form.Group>
+        {errors?.urgent?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <br />
+        <div className="text-center">
+          <Button
+            className={btnStyles.Button}
+            onClick={() => history.goBack()}
+          >
+            cancel
+          </Button>
+          <Button className={btnStyles.Button} type="submit">
+            create
+          </Button>
+        </div>
+      </Form>
+      <br />
+    </Container>
   );
 }
 
