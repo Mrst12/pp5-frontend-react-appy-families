632180cbdfbad4ed015b38505ac81611e0a8a1df
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2023-01-04 14:30:05 +0000

    make changes to allow deployment to render

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://appy-families-drf-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://appy-families-drf.onrender.com';
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
